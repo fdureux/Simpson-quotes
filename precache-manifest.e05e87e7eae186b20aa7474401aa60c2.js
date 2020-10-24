30d159297dfbbc194390069b65bb153106f75b48
fdureux <fanny.dureux@gmail.com>
2020-10-24 21:50:10 +0200

    Updates

---
+++ precache-manifest.e05e87e7eae186b20aa7474401aa60c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00182983a20824e441b077f4b7e3d8d4",
+    "url": "/simpson-quotes/index.html"
+  },
+  {
+    "revision": "8241ac9249c1a9d24a52",
+    "url": "/simpson-quotes/static/css/main.9b5f8ae9.chunk.css"
+  },
+  {
+    "revision": "8b2c6019ab37b4b377db",
+    "url": "/simpson-quotes/static/js/2.a4ee69ab.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/simpson-quotes/static/js/2.a4ee69ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8241ac9249c1a9d24a52",
+    "url": "/simpson-quotes/static/js/main.aee69b5d.chunk.js"
+  },
+  {
+    "revision": "d38d32fbe1f4a3825131",
+    "url": "/simpson-quotes/static/js/runtime-main.912994d1.js"
+  }
+]);
